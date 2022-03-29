4f5e9fb887d45d5404589b118b341641dafc3cce
LauHuiQi <52143482+LauHuiQi@users.noreply.github.com>
2022-03-29 12:05:43 +0800

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,6 @@
+a = input("Please enter a number: ")
+b = input("Please enter another number: ")
+
+average = (int(a)+int(b))/2
+
+print("\nthe average of this 2 number are " + str(average))
---
+++ Q3.py
@@ -0,0 +1,10 @@
+moduleCode = "CSC1009"
+
+if (moduleCode == "CSC1009"):
+    print("Object-Oriented Programming")
+elif (moduleCode == "CSC1008"):
+    print("Data Structure and Algorithm")
+elif (moduleCode == "CSC1007"):
+    print("Operating System")
+elif (moduleCode == "CSC1006"):
+    print("Mathematics 2")
